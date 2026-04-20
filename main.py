61b721791a9fefd8ca065369c9e3adfeccc67f8d
D-Malachi <dijonr@umich.edu>
2026-04-20 10:28:34 -0400

    Added check word function

--- main.py
+++ main.py
@@ -23,6 +23,50 @@ def get_definitions(word_list):
 
     return list_with_defs
 
+def check_guess(secret_word, guess):
+    secret_word = secret_word.lower()
+    guess = guess.lower()
+
+    if len(guess) != len(secret_word):
+        print("Guess must be of the same length as the word")
+        return None
+
+    feedback = [""] * len(secret_word)
+    used_positions = [False] * len(secret_word)
+
+    #For correct position
+    for i in range(len(guess)):
+        if guess[i] in secret_word:
+            feedback[i] = "G"
+            used_positions[i] = True
+
+    #For correct letter wrong position
+    for i in range(len(guess)):
+        if feedback[i] == "":
+            for j in range(len(secret_word)):
+                if guess[i] == secret_word[j] and not used_positions[j]:
+                    feedback[i] = "Y"
+                    used_positions[j] = True
+                    break
+
+            if feedback[i] == "":
+                feedback[i] = "X"
+
+    return feedback
+
+#Display right/wrong
+def display_feedback(guess, feedback):
+    for i in range(len(feedback)):
+        if feedback[i] == "G":
+            print(f"[{guess[i]}]", end=" ")
+        elif feedback[i] == "Y":
+            print(f"[{guess[i]}]", end=" ")
+        else:
+            print(f"[{guess[i]}]", end=" ")
+    print()
+
+
+
 # TESTING: Load words from CSV, then get definitions
 if __name__ == "__main__":
 
@@ -50,6 +94,19 @@ if __name__ == "__main__":
         words_with_defs = get_definitions(words)
         print(f"\nTotal words with definitions: {len(words_with_defs)}")"""
 
+#Testing word check and display
+secret_word = "apple"
 
+while True:
+    guess = input("\nGuess or quit: ").strip().lower()
+
+    if guess == "quit":
+        break
+
+    result = check_guess(secret_word, guess)
+
+    if result:
+        print("Feedback:", result)
+        display_feedback(guess, result)
 
 
